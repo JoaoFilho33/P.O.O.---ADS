16c4cc6dc56123011daa150d3486102d7812a8a2
João de Deus <joaod.almeidafi@gmail.com>
2022-10-04 22:41:04 -0300

    Adicionando comentário a função

--- atividade03/q04.ts
+++ atividade03/q04.ts
@@ -1,14 +1,14 @@
-const array = [1, 2, 3, 4]
-let output: string = ''
-let count = 1
+let array: number[] = [1, 2, 3, 4];
+let output: string = '';
+let count: number = 1
 
-array.forEach((num: number) => {
-    if (count < array.length) {//count usado para quando chegar na última pos. do array o último traço não seja adicionado
-        output += `${num}-`
+array.forEach(function(array) {
+    if (count < 4) {//count usado para quando chegar na última pos. do array o último traço não seja adicionado
+        output += `${array}-`
         count++
     }
     else {
-        output += `${num}`
+        output += `${array}`
     }
 })
 
